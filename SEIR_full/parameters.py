e3aee06294ea636ebafc4ee611891df352911a2b
The Weiss ;) <amitdweiss@gmail.com>
2020-04-18 04:32:47 +0300

    merged all changes with matan! fixed parameters to load new files. fixed empty cells nan bug for behave model as well

--- SEIR_full/parameters.py
+++ SEIR_full/parameters.py
@@ -15,20 +15,32 @@ import os
 
 # Beta_home - home transmissibility:
 with open('../Data/parameters/beta_home.pickle', 'rb') as pickle_in:
-    beta_home = pickle.load(pickle_in)
+	beta_home = pickle.load(pickle_in)
 
 # hospitalizations data for high risk with/without treatment and low risk:
 with open('../Data/parameters/hospitalization.pickle', 'rb') as pickle_in:
-    hospitalizations = pickle.load(pickle_in)
+	hospitalizations = pickle.load(pickle_in)
 
 
 # Asymptomatic fraction
 with open('../Data/parameters/f0_full.pickle', 'rb') as pickle_in:
-    f0_full = pickle.load(pickle_in)
+	f0_full = pickle.load(pickle_in)
 
 # Contact matrix dic
 with open('../Data/parameters/C_calibration.pickle', 'rb') as pickle_in:
-    C_calibration = pickle.load(pickle_in)
+	C_calibration = pickle.load(pickle_in)
+
+# Orthodox distribution
+with open("Data/parameters/orthodox_dist.pickle", 'rb') as pickle_in:
+	is_haredi = pickle.load(pickle_in)
+
+# stay home index for behavior model
+with open('Data/parameters/stay_home_idx.pickle', 'rb') as pickle_in:
+	stay_home_idx = pickle.load(pickle_in)
+
+# routine vector behavior model
+with open('Data/parameters/routine_t.pickle', 'rb') as pickle_in:
+    not_routine = pickle.load(pickle_in)
 
 #  gama - transition rate between Is,Ia to R
 gama = 1. / 7.
@@ -42,7 +54,7 @@ sigma = 1. / 3.5
 
 # Population size
 with open('../Data/parameters/init_pop.pickle', 'rb') as pickle_in:
-    population_size = pickle.load(pickle_in)
+	population_size = pickle.load(pickle_in)
 
 # fixing parameter for beta_home
 psi = 1
@@ -52,7 +64,7 @@ nu = 1. / 9.
 
 # Epsilon (small noise) - only for non-zero population groups
 with open('../Data/parameters/eps_dict.pickle', 'rb') as pickle_in:
-    eps = pickle.load(pickle_in)
+	eps = pickle.load(pickle_in)
 
 # alpha - early infected infection factor
 alpha = 1.0
